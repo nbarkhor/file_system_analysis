a3b8aa7413f15ff5c4afa5227b0ce64157f9a861
David Weitz <dwgiants25@gmail.com>
2018-03-10 12:04:34 -0800

    Added test cases and block validity checking

--- lab3b.py
+++ lab3b.py
@@ -2,6 +2,7 @@ import csv
 import sys
 
 superblock = []
+numBlocks=0
 group = []
 free_blocks = []
 free_inodes = []
@@ -11,13 +12,14 @@ indirects = []
 doubleindirects = []
 tripleindirects = []
 
-
 def populateArrays(filename):
     with open(filename, newline='') as csvfile:
         csvreader = csv.reader(csvfile)
         for row in csvreader:
+            global numBlocks, superblock, group, free_blocks, free_inodes, inodes, directories, indirects, doubleindirects, tripleindirects
             if row[0] == "SUPERBLOCK":
                 superblock.append(row)
+                numBlocks=int(superblock[0][1])
             elif row[0] == "GROUP":
                 group.append(row)
             elif row[0] == "BFREE":
@@ -35,6 +37,26 @@ def populateArrays(filename):
             elif row[0] == "INDIRECT" and row[2] == "3":
                 tripleindirects.append(row)
 
+def findInvalidBlocks():
+    first_unreserved_block = int(group[0][8]) + int((int(superblock[0][2])*int(superblock[0][4])/int(superblock[0][3]))) #int(superblock[0][6]) + 4 #superblock+group+block bitmap + inode bitmap +inodes
+    for inode in inodes:
+        for x in range (12,24):
+            if int(inode[x])<0 or int(inode[x])>numBlocks:
+                print ("INVALID BLOCK {0} IN INODE {1} AT OFFSET {2}".format(inode[x],inode[1],x-12))
+            elif int(inode[x])>0 and int(inode[x])<first_unreserved_block:
+                print ("RESERVED BLOCK {0} IN INODE {1} AT OFFSET {2}".format(inode[x],inode[1],x-12))
+        if int(inode[24])<0 or int(inode[24])>numBlocks:
+            print ("INVALID INDIRECT BLOCK {0} IN INODE {1} AT OFFSET 12".format(inode[24],inode[1]))
+        elif int(inode[24])>0 and int(inode[24])<first_unreserved_block:
+            print ("RESERVED INDIRECT BLOCK {0} IN INODE {1} AT OFFSET 12".format(inode[24],inode[1]))
+        if int(inode[25])<0 or int(inode[25])>numBlocks:
+            print ("INVALID DOUBLE INDIRECT BLOCK {0} IN INODE {1} AT OFFSET 268".format(inode[25],inode[1]))
+        elif int(inode[25])>0 and int(inode[25])<first_unreserved_block:
+            print ("RESERVED DOUBLE INDIRECT BLOCK {0} IN INODE {1} AT OFFSET 268".format(inode[25],inode[1]))
+        if int(inode[26])<0 or int(inode[26])>numBlocks:
+            print ("INVALID TRIPLE INDIRECT BLOCK {0} IN INODE {1} AT OFFSET 65804".format(inode[26],inode[1]))
+        elif int(inode[26])>0 and int(inode[26])<first_unreserved_block:
+            print ("RESERVED TRIPLE INDIRECT BLOCK {0} IN INODE {1} AT OFFSET 65804".format(inode[26],inode[1]))
 
 
 if __name__ == "__main__":
@@ -43,8 +65,4 @@ if __name__ == "__main__":
          exit(1)
     filename = sys.argv[1]
     populateArrays(filename)
-    print (superblock)
-    print(group)
-    print(indirects)
-    print(doubleindirects)
-    print(tripleindirects)
+    findInvalidBlocks()
