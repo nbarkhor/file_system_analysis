27d2fd9a7666d479897bee7c0ff6979d7d73a663
David Weitz <dwgiants25@gmail.com>
2018-03-09 21:57:52 -0800

    File parsing working

--- lab3b.py
+++ lab3b.py
@@ -1,10 +1,50 @@
 import csv
-import argparse
+import sys
 
-parser = argparse.ArgumentParser()
+superblock = []
+group = []
+free_blocks = []
+free_inodes = []
+inodes = []
+directories = []
+indirects = []
+doubleindirects = []
+tripleindirects = []
 
-with open('eggs.csv', 'wb') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=' ',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
-    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+
+def populateArrays(filename):
+    with open(filename, newline='') as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            if row[0] == "SUPERBLOCK":
+                superblock.append(row)
+            elif row[0] == "GROUP":
+                group.append(row)
+            elif row[0] == "BFREE":
+                free_blocks.append(row)
+            elif row[0] == "IFREE":
+                free_inodes.append(row)
+            elif row[0] == "INODE":
+                inodes.append(row)
+            elif row[0] == "DIRENT":
+                directories.append(row)
+            elif row[0] == "INDIRECT" and row[2] == "1":
+                indirects.append(row)
+            elif row[0] == "INDIRECT" and row[2] == "2":
+                doubleindirects.append(row)
+            elif row[0] == "INDIRECT" and row[2] == "3":
+                tripleindirects.append(row)
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv)!=2:
+         sys.stderr.write('Need to pass file name as an argument\n')
+         exit(1)
+    filename = sys.argv[1]
+    populateArrays(filename)
+    print (superblock)
+    print(group)
+    print(indirects)
+    print(doubleindirects)
+    print(tripleindirects)
